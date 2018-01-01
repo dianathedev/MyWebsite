c75f8da5fa178de93536d2da634e5fd16581acef
Dianathedev <Dianathedev@gmail.com>
2017-12-31 16:30:57 -0800

    Grid is working

--- js/index.js
+++ js/index.js
@@ -1,61 +1,32 @@
 // shoot js when DOM is loaded
 $(document).ready(function() {
-
+  
     // setting global variables
-    var recentTrackUrl = "https://ws.audioscrobbler.com/2.0?method=user.getRecentTracks&user=dianathedev&limit=200&api_key=a8de5ee9631ba5026c38ead04e2e47a7&format=json";
+    var recentTrackUrl = "https://ws.audioscrobbler.com/2.0?method=user.getRecentTracks&user=dianathedev&limit=100&api_key=a8de5ee9631ba5026c38ead04e2e47a7&format=json";
     var defaultCoverImage = "https://cdn.last.fm/flatness/responsive/2/noimage/default_album_300_g4.png";
-    var refreshTime = 50; //look for updated JSON every "xxx" miliseconds
-
-    //defining the function which gets JSON data from the last.fm api and sets it to the
+    //var refreshTime = 5000; //look for updated JSON every "xxx" miliseconds
+    
+    //definig the function which gets JSON data from the last.fm api and sets it to the 
     function setRecentTrack() {
         $.getJSON(recentTrackUrl, function(data){
-            var info = data.recenttracks.track[0];
-                var track = info.name;
-                var artist = info.artist["#text"];
-                var album = info.album["#text"];
-                var cover = info.image[3]["#text"];
-                var url = info.url;
-                //var nowplaying = info["@attr"];
 
-            if ( cover.length > 0 ) {
-                $(".cover").attr("src", cover);
-                $("body, .blurred-background").css("background-image", "url(" + cover + ")");
+            $.each(data.recenttracks.track, function(i, info){
+                var cover = info.image[3]["#text"]; 
+
+                if(cover.length > 0) {
+                    $('<div/>', {
+                        'class': 'square',
+                        'css': {
+                            'background-image': "url('" + cover + "')"
+                        }
+                    }).appendTo('#root');
                 }
-            else {
-                $(".cover").attr("src", defaultCoverImage);
-                $("body, .blurred-background").css("background-image", "url(" + defaultCoverImage + ")");
-            }
-
-            var abDefaults = {
-              normalizeTextColor: true,
-              normalizedTextColors: {
-                light: "rgba(255,255,255,0.7)",
-                dark:  "rgba(0,0,0,0.7)"
-              },
-            }
-
-            // $(".cover-container").attr("href", url);
-            // $.adaptiveBackground.run(abDefaults);
-            // $(".cover").attr("title", "Cover of \"" + album + "\" by " + artist);
-            // $(".artist").html(artist);
-            // $(".track").html(track);
-            // $(".album").html(album);
-
-            if (nowplaying) {
-                $(".time").html("is listening to right now");
-            }
-            else {
-                var timestamp = parseFloat(info.date.uts);
-                var timeISO = new Date(timestamp * 1000).toISOString();
-                $(".time").html("was listening to ");
-                $("time").attr("datetime", timeISO);
-                $("time.timeago").timeago();
-            }
+            });
         });
-    }
+    } 
     //run function on pageload then load on time interval
     setRecentTrack();
-    setInterval(setRecentTrack, refreshTime);
+    //setInterval(setRecentTrack, refreshTime);       
 
-
+    
 });
